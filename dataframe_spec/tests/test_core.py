b50eacf0ed04c80667c99cc425d7c03dd622c96e
Thomas A Caswell <tcaswell@bnl.gov>
2020-03-12 00:03:47 -0400

    TST: the full abc.Mapping API

--- dataframe_spec/tests/test_core.py
+++ dataframe_spec/tests/test_core.py
@@ -24,13 +24,40 @@ class TestDictLike:
             assert n in baseline
 
     def test_items(self, df_factory, baseline, col_convert):
+        "This tests that .items() works as expected"
         df = df_factory(baseline)
         for (tk, tv) in df.items():
             assert np.all(col_convert(tv) == baseline[tk])
 
+    def test_values(self, df_factory, baseline, col_convert):
+        "This tests that .values() works as expected"
+        df = df_factory(baseline)
+        for tv in df.values():
+            # so far just a smoke test
+            col_convert(tv)
+
+    def test_keys(self, df_factory, baseline, col_convert):
+        "This tests that .keys() works as expected"
+        df = df_factory(baseline)
+        assert set(df.keys()) == set(baseline.keys())
+
+    def test_len(self, df_factory, baseline):
+        "This tests that length follows abc.Mapping semantics"
+        df = df_factory(baseline)
+        assert len(df) == len(baseline)
+        assert len(df) == len(list(df))
+
+    @pytest.mark.xfail(reason="Equality is hard")
+    def test_eq(self, df_factory, baseline):
+        df1 = df_factory(baseline)
+        df2 = df_factory(baseline)
+        assert df1 == df2
+        assert not df1 != df2
+
 
 class TestCols:
-    def test_len(self, df_factory, baseline):
+    def test_col_len(self, df_factory, baseline):
+        "This tests that all columns are the same length"
         df = df_factory(baseline)
         first_col_len = None
         for n in df:
